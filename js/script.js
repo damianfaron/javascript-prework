4446a183dc0e757d029c4f401cc293062636f439
Damian Faron <damian.faron@onet.pl>
2025-06-18 14:18:18 +0200

    create variables to js

---
+++ js/script.js
@@ -0,0 +1,12 @@
+let computerMove;
+let playerMove;
+computerMove = 'kamień';
+playerMove = 'papier';
+
+printMessage(
+	'Zagrałem ' +
+		computerMove +
+		'! Jeśli Twój ruch to ' +
+		playerMove +
+		', to wygrasz!'
+);
