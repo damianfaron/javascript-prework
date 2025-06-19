624114fafacdd4db7e9bca92f13d4d0fc91ee9aa
Damian Faron <damian.faron@onet.pl>
2025-06-19 10:11:25 +0200

    create logic js to game

--- js/script.js
+++ js/script.js
@@ -1,8 +1,35 @@
 let computerMove;
 let playerMove;
-computerMove = 'kamień';
-playerMove = 'papier';
+let randomNumber;
+let playerInput;
 
+// computer
+computerMove = randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+
+// player
+playerMove = playerInput;
+playerInput = prompt('Wybier swój ruch! 1: Kamineń, 2: Papier, 3: nożyce');
+
+console.log('Wylosowana liczba to ' + randomNumber);
+if (randomNumber == '1') {
+	computerMove = 'Kamień';
+} else if (randomNumber == '2') {
+	computerMove = 'Papier';
+} else if (randomNumber == '3') {
+	computerMove = 'Nożyce';
+}
+
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+	playerMove = 'Kamień';
+} else if ((playerInput = '2')) {
+	playerMove = 'Papier';
+} else if ((playerInput = '3')) {
+	playerMove = 'Nożyce';
+}
+
+printMessage('mój ruch: ' + computerMove);
 printMessage(
 	'Zagrałem ' +
 		computerMove +
